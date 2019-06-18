6844b25ab98e7bdcc671e110a8a46e4eac96c5a7
Ojaswi Wadhwa <ojas.vwadhwa1995@gmail.com>
2019-06-18 13:35:58 +0530

    complete

--- src/app/left-side/left-side.component.ts
+++ src/app/left-side/left-side.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftSideComponent implements OnInit {
   navHeaders = [
-    {name: 'Company', active: false},
+    {name: 'Company', active: true},
     {name: 'Investor', active: false},
     {name: 'Person', active: false},
     {name: 'Sector', active: false},
@@ -33,9 +33,88 @@ export class LeftSideComponent implements OnInit {
     {show: false, active: false, title: 'List of public comparables for a company ?', tag: this.tags[1]},
     {show: false, active: false, title: 'What is the fast path to a company ?', tag: this.tags[4]},
   ];
+
+  sectorData1 = [
+    {active: false, icon: 'fa icon-color fa-building mr-10 mt-5', title: 'Enterprise infrastructure',
+      sectorTags: [
+        {name: 'Enterprise Networking'},
+        {name: 'Enterprise Servers & Systems'},
+        {name: 'Enterprise Storage'},
+        {name: 'Application development & Integration'},
+        {name: 'Cyber security'},
+        {name: 'Enterprise IT Operations Management'}
+      ]
+    },
+    {active: false, icon: 'fa icon-color fa-wrench mr-10 mt-5', title: 'Industrial Tech', sectorTags: [
+      {name: '3D Printing'},
+      {name: 'Auto Tech'},
+      {name: 'Drones'},
+      {name: 'Management Tech'},
+      {name: 'Robotics'},
+      {name: 'Utilities Tech'},
+      {name: 'IoT - Industrial'},
+      {name: 'IoT - Auto'},
+      {name: 'IoT - Utilities'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-stopwatch mr-10 mt-5', title: 'AI & analytics', sectorTags: [
+      {name: 'Artificial Intelligence'},
+      {name: 'Data Management & Analytics'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-flag mr-10 mt-5', title: 'Rail & E-commerce tech', sectorTags: [
+      {name: 'Retail Tech'},
+      {name: 'E-commerce Tech'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-plane mr-10 mt-5', title: 'Travel and hospitality', sectorTags: [
+      {name: 'Travel Tech'},
+      {name: 'Transport Tech'},
+      {name: 'Hospitality Tech'}
+    ]}
+  ];
+
+  sectorData2 = [
+    {active: false, icon: 'fa icon-color fa-tv mr-10 mt-5', title: 'Enterprise software', sectorTags: [
+      {name: 'AdTech'},
+      {name: 'AR/VR'},
+      {name: 'BlockChain'},
+      {name: 'Digital Marketing'},
+      {name: 'Financial Management Tech'},
+      {name: 'Human Capital Management Tech'},
+      {name: 'Sales & CRM Software'},
+      {name: 'Supply Chain Management'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-industry mr-10 mt-5', title: 'FinTech', sectorTags: [
+      {name: 'Alternative Lending & Financing'},
+      {name: 'Banking Tech'},
+      {name: 'Capital Markets Tech'},
+      {name: 'Financial Data &amp; Analytics'},
+      {name: 'Insurance Tech - Auto'},
+      {name: 'Insurance Tech - Life & Health'},
+      {name: 'Insurance Tech - Property & Casuality'},
+      {name: 'Payment & Transfer'},
+      {name: 'RegTech'},
+      {name: 'WealthTech'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-globe mr-10 mt-5', title: 'Internet of Things', sectorTags: [
+      {name: 'IoT - Device & Platforms'},
+      {name: 'IoT - Auto'},
+      {name: 'IoT - Commercial Buildings'},
+      {name: 'IoT - Customer'},
+      {name: 'IoT - Industrial'},
+      {name: 'IoT - Smart Cities'},
+      {name: 'IoT - Utilities'}
+    ]},
+    {active: false, icon: 'fa icon-color fa-box-open mr-10 mt-5', title: 'Other verticals', sectorTags: [
+      {name: 'Education Tech'},
+      {name: 'Legal Tech'},
+      {name: 'Ag Tech'}
+    ]}
+  ];
+
   switchHead;
   switchTag;
+  switchSector;
   switchTagData;
+  activeTab = 'Company';
   constructor() { }
 
   ngOnInit() {
@@ -47,6 +126,7 @@ export class LeftSideComponent implements OnInit {
       return x.name === head.name;
     });
     this.switchHead.active = true;
+    this.activeTab = this.switchHead.name
     console.log(this.switchHead);
   }
 
@@ -65,6 +145,21 @@ export class LeftSideComponent implements OnInit {
       data.show = false;
     });
     console.log(this.switchTag);
+  }
+
+  toogleSector(sector){
+    this.resetSector();
+
+    this.switchSector = this.sectorData1.find((x) => {
+      return x.title === sector.title;
+    });
+    if(!this.switchSector){
+      this.switchSector = this.sectorData2.find((x) => {
+        return x.title === sector.title;
+      });
+    }
+    this.switchSector.active = false;
+    console.log(this.switchSector);
   }
 
   resetHeader() {
@@ -96,6 +191,84 @@ export class LeftSideComponent implements OnInit {
       {show: true, active: false, title: 'List of industry segments that a company operats in ?', tag: this.tags[0]},
       {show: true, active: false, title: 'List of public comparables for a company ?', tag: this.tags[1]},
       {show: true, active: false, title: 'What is the fast path to a company ?', tag: this.tags[4]},
+    ];
+  }
+
+  resetSector() {
+    this.sectorData1 = [
+      {active: true, icon: 'fa icon-color fa-building mr-10 mt-5', title: 'Enterprise infrastructure',
+        sectorTags: [
+          {name: 'Enterprise Networking'},
+          {name: 'Enterprise Servers & Systems'},
+          {name: 'Enterprise Storage'},
+          {name: 'Application development & Integration'},
+          {name: 'Cyber security'},
+          {name: 'Enterprise IT Operations Management'}
+        ]
+      },
+      {active: true, icon: 'fa icon-color fa-wrench mr-10 mt-5', title: 'Industrial Tech', sectorTags: [
+        {name: '3D Printing'},
+        {name: 'Auto Tech'},
+        {name: 'Drones'},
+        {name: 'Management Tech'},
+        {name: 'Robotics'},
+        {name: 'Utilities Tech'},
+        {name: 'IoT - Industrial'},
+        {name: 'IoT - Auto'},
+        {name: 'IoT - Utilities'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-stopwatch mr-10 mt-5', title: 'AI & analytics', sectorTags: [
+        {name: 'Artificial Intelligence'},
+        {name: 'Data Management & Analytics'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-flag mr-10 mt-5', title: 'Rail & E-commerce tech', sectorTags: [
+        {name: 'Retail Tech'},
+        {name: 'E-commerce Tech'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-plane mr-10 mt-5', title: 'Travel and hospitality', sectorTags: [
+        {name: 'Travel Tech'},
+        {name: 'Transport Tech'},
+        {name: 'Hospitality Tech'}
+      ]}
+    ];
+  
+    this.sectorData2 = [
+      {active: true, icon: 'fa icon-color fa-tv mr-10 mt-5', title: 'Enterprise software', sectorTags: [
+        {name: 'AdTech'},
+        {name: 'AR/VR'},
+        {name: 'BlockChain'},
+        {name: 'Digital Marketing'},
+        {name: 'Financial Management Tech'},
+        {name: 'Human Capital Management Tech'},
+        {name: 'Sales & CRM Software'},
+        {name: 'Supply Chain Management'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-industry mr-10 mt-5', title: 'FinTech', sectorTags: [
+        {name: 'Alternative Lending & Financing'},
+        {name: 'Banking Tech'},
+        {name: 'Capital Markets Tech'},
+        {name: 'Financial Data &amp; Analytics'},
+        {name: 'Insurance Tech - Auto'},
+        {name: 'Insurance Tech - Life & Health'},
+        {name: 'Insurance Tech - Property & Casuality'},
+        {name: 'Payment & Transfer'},
+        {name: 'RegTech'},
+        {name: 'WealthTech'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-globe mr-10 mt-5', title: 'Internet of Things', sectorTags: [
+        {name: 'IoT - Device & Platforms'},
+        {name: 'IoT - Auto'},
+        {name: 'IoT - Commercial Buildings'},
+        {name: 'IoT - Customer'},
+        {name: 'IoT - Industrial'},
+        {name: 'IoT - Smart Cities'},
+        {name: 'IoT - Utilities'}
+      ]},
+      {active: true, icon: 'fa icon-color fa-box-open mr-10 mt-5', title: 'Other verticals', sectorTags: [
+        {name: 'Education Tech'},
+        {name: 'Legal Tech'},
+        {name: 'Ag Tech'}
+      ]}
     ];
   }
 
